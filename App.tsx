bb45e93886b4198011bed18a72ddce1e18d1a177
Maya Pasricha <shrey.pasricha@gmail.com>
2024-01-14 10:21:59 -0500

    Fix map and add icons.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import HomeScreen from "./src/home";
 import ChallengesScreen from "./src/challenges";
 import ProfileScreen from "./src/profile";
+import MapScreen from "./src/map";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Home') {
+                            iconName = focused
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline';
+                        } else if (route.name === 'Settings') {
+                            iconName = focused ? 'ios-list' : 'ios-list-outline';
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
                 <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Challeges" component={ChallengesScreen} />
-                <Tab.Screen name="Profiles" component={ProfileScreen} />
+                <Tab.Screen name="Challenges" component={ChallengesScreen} />
+                <Tab.Screen name="Map" component={MapScreen} />
+                <Tab.Screen name="Profile" component={ProfileScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     );
---
+++ src/map.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {enableLatestRenderer} from 'react-native-maps';
+/* import React from 'react'; */
+import MapView from 'react-native-maps';
+
+enableLatestRenderer();
+
+export default function MapScreen() {
+    return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
