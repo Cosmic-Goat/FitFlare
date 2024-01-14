ceb16c7eb976fb2fd250a3db81707b60923b0efb
Maya Pasricha <shrey.pasricha@gmail.com>
2024-01-13 20:07:02 -0500

    Add navigation bar.

--- test.tsx
+++ test.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+export default function Test() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#282828',
     alignItems: 'center',
     justifyContent: 'center',
   },
